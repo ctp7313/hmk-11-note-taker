e69586077d317848208aa7200448312dca5c8398
Chris Parker <ctp7313@gmail.com>
2020-11-08 19:29:04 -0700

    Push for submission

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# hmk-11-note-taker
+# Homework 11 - Note Taker
+
+![GitHub License](https://img.shields.io/badge/License-MIT-green.svg)
+
+## Description
+
 An note taking app using Express. A user will be able to save a titled note and then see it saved to a sidebar for later use.
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#Questions)
+
+## Installation
+
+To install necessary dependecies, run the following commands:
+
+```
+npm i
+```
+
+## Usage
+
+N/A
+
+## License
+
+This project is licensed under the MIT license.
+
+## Contributing
+
+N/A
+
+## Tests
+
+To run tests, run the following command:
+```
+
+```
+
+## Questions
+
+If you have any questions, open an issue or contact me directly at [ctp7313@gmail.com](ctp7313@gmail.com). You can find more of my work at [ctp7313](https://github.com/ctp7313)
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,56 @@
-const dbJson = require('../db/db.json');
-const fs = require('fs');
-// const express = require('express');
-// const app = express()
+const dbJSON = require("../db/db.json");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 module.exports = function (app) {
-
-  app.get('/api/notes', function (req, res) {
-    res.json(dbJson);
-  });
-
-  app.post('/api/notes', function (req, res) {
-    const newNote = req.body;
-    fs.readFile(dbJson, (err, data) => {
-      if (err) throw err;
-      console.log(data);
+  app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+      let notes = JSON.parse(data);
+      res.json(notes);
     });
-    
-
-    fs.writefile(dbJson, (data, err) => {
-      if (err) throw err;
-      res.json(data)
-    }
-   )});
-
-  app.delete('/api/notes', function (req, res) {
-
   });
 
+  app.post("/api/notes", function (req, res) {
+    let newNote = req.body;
+    newNote.id = uuidv4();
+    let allNotes = [];
+
+    fs.readFile("./db/db.json", function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+      allNotes = JSON.parse(data);
+      allNotes.push(newNote);
+
+      fs.writeFile("./db/db.json", JSON.stringify(allNotes), function (err) {
+        if (err) {
+          console.log(err);
+        }
+        return res.json(allNotes);
+      });
+    });
+  });
+
+  app.delete("/api/notes/:id", function (req, res) {
+    let noteId = req.params.id;
+
+    fs.readFile("./db/db.json", function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+      const notesArr = JSON.parse(data);
+      console.log(notesArr);
+      const idFilter = notesArr.filter((entry) => entry.id !== noteId);
+
+      fs.writeFile("./db/db.json", JSON.stringify(idFilter), function (err) {
+        if (err) {
+          console.log(err);
+        }
+        return res.json(dbJSON);
+      });
+    });
+  });
 };
